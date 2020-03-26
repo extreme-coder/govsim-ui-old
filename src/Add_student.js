2cc1077681942eddeb15c43baec43c268aa99a5e
CrazzyGamer182 <nirmay.singh.lamba@gmail.com>
2020-03-26 16:07:56 -0700

    Added form

---
+++ src/Add_student.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Form, Button} from 'react-bootstrap';
+
+class Home extends React.Component {
+    render() {
+      return (
+        <div>
+             <h1>Create new student</h1>
+             <Form>
+             <Form.Group controlId="formBasicName">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control type="First name" placeholder="Enter first name" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicLastName">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control type="Last name" placeholder="Enter last name" />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicDate">
+                    <Form.Label>Date of birth</Form.Label>
+                    <Form.Control type="Date of birth" placeholder="Enter date of birth" />
+                    </Form.Group>
+
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Create
+                </Button>
+            </Form>
+        </div>
+
+      );
+    }
+}
+
+export default Home;
