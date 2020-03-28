b79205740701aa8cace21cd41b38647569b5cef9
aryansingh37 <aryan.singh.lamba@gmail.com>
2020-03-27 23:43:50 -0700

    Linked class entry form to API

--- src/AddClass.js
+++ src/AddClass.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import {Form, Button} from 'react-bootstrap';
-
+import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import TextField from './common/TextField';
+import * as actions from './actions/classes';
 
 class AddClass extends React.Component {
-    addStudent = (values) => {
-        console.log(values)
-        //TODO: call the action to add student
+    addClass = (values) => {
+      console.log(values)
+      this.props.addClass(values)
     }
 
     render() {
       return (
         <div>
             <h3>Add Class</h3>
-            <form onSubmit={this.props.handleSubmit(this.addStudent)}>
+            <form onSubmit={this.props.handleSubmit(this.addClass)}>
                <Form.Group controlId="formBasicName">
                     
                 <Field name="Name" component={TextField} label="Name" placeholder="Enter Name" />
 
                 <Field name="StartTime" component={TextField} label="StartTime" placeholder="Enter Start Time" />
 
-                <Field name="EndTime" component={TextField} label="End Time" placeholder="Enter End Time" />
+                <Field name="Endtime" component={TextField} label="End Time" placeholder="Enter End Time" />
 
                 <Field name="StartDate" component={TextField} label="Start Date" placeholder="Enter Start Date" />
 
@@ -29,9 +30,9 @@ class AddClass extends React.Component {
 
                 <Field name="Teachers" component={TextField} label="Teachers" placeholder="Enter Teachers" />
 
-                <Field name="Room" component={TextField} label="Room" placeholder="Enter Room" />
+                <Field name="RoomForClass" component={TextField} label="Room" placeholder="Enter Room" />
 
-                <Field name="DOTW" component={TextField} label="Days Of Class" placeholder="Enter Days" />
+                <Field name="DaysOfClass" component={TextField} label="Days Of Class" placeholder="Enter Days" />
             
                 <Button variant="primary" type="submit">
                     Add
@@ -44,5 +45,8 @@ class AddClass extends React.Component {
     }
 }
 
+AddClass = connect(
+  null, actions
+)(AddClass);
 
 export default reduxForm({form: 'add_class_form', enableReinitialize: true})(AddClass);
--- src/actions/classes.js
+++ src/actions/classes.js
@@ -6,3 +6,17 @@ export const getClasses = createAction('LOAD_CLASSES', () => ({
     }
   })
 );
+
+export const addClass = createAction('ADD_CLASS', (clas) => ({
+    request: {
+      url: '/classes',
+      method: 'POST',
+      data: clas //spelled with one s so javascript doesn't get confused
+    },
+    options: {
+      onSuccess({ getState, dispatch, response }) {        
+        window.location = '/classes';
+      }
+    }
+  })
+);
