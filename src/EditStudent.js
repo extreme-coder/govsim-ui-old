4da57e015dcacb1269aa7a129175fbc607883b73
aryansingh37 <aryan.singh.lamba@gmail.com>
2020-03-27 21:14:11 -0700

    Fixed indenting on form

--- src/EditStudent.js
+++ src/EditStudent.js
@@ -7,26 +7,26 @@ class EditStudent extends React.Component {
         <div>
              <h3>Create new student</h3>
              <Form>
-             <Form.Group controlId="formBasicName">
+                <Form.Group controlId="formBasicName">
                     <Form.Label>First name</Form.Label>
                     <Form.Control type="First name" placeholder="Enter first name" />
-                    </Form.Group>
+                </Form.Group>
 
-                    <Form.Group controlId="formBasicLastName">
+                <Form.Group controlId="formBasicLastName">
                     <Form.Label>Last name</Form.Label>
                     <Form.Control type="Last name" placeholder="Enter last name" />
-                    </Form.Group>
+                </Form.Group>
 
-                    <Form.Group controlId="formBasicDate">
+                <Form.Group controlId="formBasicDate">
                     <Form.Label>Date of birth</Form.Label>
                     <Form.Control type="Date of birth" placeholder="Enter date of birth" />
-                    </Form.Group>
+                </Form.Group>
 
                 <Form.Group controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
                     <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
+                        We'll never share your email with anyone else.
                     </Form.Text>
                 </Form.Group>
 
