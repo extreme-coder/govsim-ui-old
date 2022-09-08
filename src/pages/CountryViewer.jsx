3807f3bc3b47900c41f148a354edfff19c906168
aryansingh37 <aryan.singh.lamba@gmail.com>
2022-09-08 15:27:20 -0700

    added button to create bills

--- src/pages/CountryViewer.jsx
+++ src/pages/CountryViewer.jsx
@@ -190,6 +190,43 @@ class CountryViewer extends React.Component {
                     </Row>
                   ))
                 }
+                <Row>
+                  <Popup trigger={<button type="button" className="btn btn-block" style={{ backgroundColor: 'gray', color: 'white' }}>+</button>} position="right center" modal nested>
+                    {(close) => (
+                      <div className="cvpopup">
+                        <button className="close" onClick={close}>
+                          &times;
+                        </button>
+                        <div className="header">
+                          {' '}
+                          Bill Creator
+                          {' '}
+                        </div>
+                        <div className="content">
+                          form here
+                        </div>
+                        <div className="actions">
+                          <button
+                            className="button"
+                            onClick={() => {
+                              close();
+                            }}
+                          >
+                            Save
+                          </button>
+                          <button
+                            className="button"
+                            onClick={() => {
+                              close();
+                            }}
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </Popup>
+                </Row>
               </Container>
             </div>
           </Row>
